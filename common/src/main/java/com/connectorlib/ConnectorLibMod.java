4182d27ea27e0bea65f28898014bbb26859507a4
meeplabsdev <meeplabsdev@noreply.users.github.com>
2025-04-10 23:16:44 +0100

    Switch connection to new config system

--- common/src/main/java/com/connectorlib/ConnectorLibMod.java
+++ common/src/main/java/com/connectorlib/ConnectorLibMod.java
@@ -1,52 +1,21 @@
 package com.connectorlib;
 
-import com.connectorlib.java_websocket.client.WebSocketClient;
-import com.connectorlib.java_websocket.drafts.Draft;
-import com.connectorlib.java_websocket.handshake.ServerHandshake;
-
-import java.net.URI;
-import java.net.URISyntaxException;
+import com.connectorlib.messages.IdentityRequest;
+import com.connectorlib.messages.NetworkData;
+import dev.architectury.event.events.client.ClientLifecycleEvent;
 
 public final class ConnectorLibMod {
 	public static final String MOD_ID = "connectorlib";
 	public static final String ghUser = "meeplabsdev";
 	public static final String repoName = "connectorlib-mod";
 
-	public static WebSocketClient client;
+	public static void init() {
+		ClientLifecycleEvent.CLIENT_SETUP.register(minecraftClient -> {
+			String username = minecraftClient.getSession().getUsername();
+			String uuid = minecraftClient.getSession().getUuid();
 
-	public static void init() throws URISyntaxException, InterruptedException {
-		try {
-//			client = new ConnectorClient(new URI("wss://server-general.skunk-snake.ts.net"));
-			client = new ConnectorClient(new URI("ws://localhost:3000"));
-			client.connectBlocking();
-		} catch (Exception ignored) {
-		}
-	}
-
-	public static class ConnectorClient extends WebSocketClient {
-
-		public ConnectorClient(URI serverUri, Draft draft) {
-			super(serverUri, draft);
-		}
-
-		public ConnectorClient(URI serverURI) {
-			super(serverURI);
-		}
-
-		@Override
-		public void onOpen(ServerHandshake handshakedata) {
-		}
-
-		@Override
-		public void onMessage(String message) {
-		}
-
-		@Override
-		public void onClose(int code, String reason, boolean remote) {
-		}
-
-		@Override
-		public void onError(Exception ex) {
-		}
+			ModConnector.getInstance().send(new IdentityRequest(username, uuid));
+			ModConnector.getInstance().send(new NetworkData());
+		});
 	}
 }
