5f98e50572c87edd00ca93a0707f6d09b8f66ab2
meeplabsdev <meeplabsdev@noreply.users.github.com>
2025-04-10 23:17:12 +0100

    Switch analytics to new message system

--- fabric/src/main/java/com/connectorlib/fabric/client/ConnectorLibModFabricClient.java
+++ fabric/src/main/java/com/connectorlib/fabric/client/ConnectorLibModFabricClient.java
@@ -1,40 +1,34 @@
 package com.connectorlib.fabric.client;
 
-import com.connectorlib.ConnectorLibMod;
+import com.connectorlib.ModConfig;
+import com.connectorlib.ModConnector;
+import com.connectorlib.messages.PositionData;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.util.math.Vec3d;
 
 public final class ConnectorLibModFabricClient implements ClientModInitializer {
-	private static final int MESSAGE_INTERVAL_TICKS = 3 * 20;
 	private int tickCounter = 0;
 
 	@Override
 	public void onInitializeClient() {
-		String name = MinecraftClient.getInstance().getSession().getUsername();
-		String puuid = MinecraftClient.getInstance().getSession().getUuid();
-
-		try {
-			ConnectorLibMod.client.send("1|" + puuid + "|" + name);
-		} catch (Exception ignored) {
-		}
+		if (!ModConfig.getInstance().get("analyticsEnabled").getAsBoolean()) return;
+		int analyticsPeriod = ModConfig.getInstance().get("analyticsPeriod").getAsInt() / 50;
 
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			tickCounter++;
-			if (tickCounter >= MESSAGE_INTERVAL_TICKS) {
-				if (client.player != null && client.getCurrentServerEntry() != null) {
+			if (tickCounter >= analyticsPeriod) {
+				if (client.player != null) {
 					ClientPlayerEntity player = client.player;
-					String ip = client.getCurrentServerEntry().address;
-					String suuid = player.getUuidAsString();
 					Vec3d pos = player.getPos();
-					String location = (int) pos.x + "," + (int) pos.y + "," + (int) pos.z;
 
-					try {
-						ConnectorLibMod.client.send("2|" + ip + "|" + suuid + "|" + location);
-					} catch (Exception ignored) {
+					String ip = "unknown";
+					if (client.getCurrentServerEntry() != null) {
+						ip = client.getCurrentServerEntry().address;
 					}
+
+					ModConnector.getInstance().send(new PositionData(ip, (int) pos.x, (int) pos.y, (int) pos.z));
 				}
 
 				tickCounter = 0;
