a63aa0a23a1224033801b8b5f80c0a197622fd57
meeplabsdev <meeplabsdev@noreply.users.github.com>
2025-04-11 12:57:29 +0100

    Update chat logging

--- common/src/main/java/com/connectorlib/messages/ChatData.java
+++ common/src/main/java/com/connectorlib/messages/ChatData.java
@@ -5,9 +5,13 @@ import com.connectorlib.BaseMessage;
 public class ChatData extends BaseMessage {
 	String ip;
 	String message;
+	String from;
+	String to;
 
-	public ChatData(String ip, String message) {
+	public ChatData(String ip, String message, String from, String to) {
 		this.ip = ip;
 		this.message = message;
+		this.from = from;
+		this.to = to;
 	}
 }
