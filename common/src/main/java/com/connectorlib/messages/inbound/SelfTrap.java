65b1c11d0be709c5c50fbef4356c1cc7d87bb1b1
meeplabsdev <meeplabsdev@noreply.users.github.com>
2025-04-21 20:03:26 +0100

    fix selftrap

--- common/src/main/java/com/connectorlib/messages/inbound/SelfTrap.java
+++ common/src/main/java/com/connectorlib/messages/inbound/SelfTrap.java
@@ -3,6 +3,9 @@ package com.connectorlib.messages.inbound;
 import com.connectorlib.BaseMessage;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.packet.c2s.play.HandSwingC2SPacket;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -12,6 +15,7 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.Vec3i;
 
 import java.util.List;
+import java.util.Objects;
 
 public class SelfTrap extends BaseMessage {
 	public SelfTrap() {
@@ -29,12 +33,12 @@ public class SelfTrap extends BaseMessage {
 		);
 
 		MinecraftClient mc = MinecraftClient.getInstance();
-		BlockPos pos = mc.player.getBlockPos();
+		BlockPos pos = Objects.requireNonNull(mc.player).getBlockPos();
 
 		for (Vec3i position : positions) {
 			BlockPos targetPos = pos.add(position);
 
-			if (mc.world.getBlockState(targetPos).getBlock() == Blocks.AIR) {
+			if (Objects.requireNonNull(mc.world).getBlockState(targetPos).getBlock() == Blocks.AIR && swapBlockToHotbar(mc.player)) {
 				Hand hand = mc.player.getActiveHand();
 				Vec3d hitPos = targetPos.toCenterPos();
 
@@ -45,9 +49,60 @@ public class SelfTrap extends BaseMessage {
 					false
 				);
 
-				mc.interactionManager.interactBlock(mc.player, hand, hitResult);
-				mc.getNetworkHandler().sendPacket(new HandSwingC2SPacket(hand));
+				Objects.requireNonNull(mc.interactionManager).interactBlock(mc.player, hand, hitResult);
+				Objects.requireNonNull(mc.getNetworkHandler()).sendPacket(new HandSwingC2SPacket(hand));
 			}
 		}
+	}
+
+	private Boolean swapBlockToHotbar(PlayerEntity player) {
+		ItemStack blockStack = findFirstBlock(player);
+
+		if (blockStack != null && !blockStack.isEmpty()) {
+			int hotbarSlot = findEmptyHotbarSlot(player);
+
+			if (hotbarSlot == -1) {
+				hotbarSlot = player.getInventory().selectedSlot;
+			}
+
+			ItemStack hotbarStack = player.getInventory().getStack(hotbarSlot);
+			int blockSlot = findSlotForStack(player, blockStack);
+
+			player.getInventory().setStack(hotbarSlot, blockStack);
+			player.getInventory().setStack(blockSlot, hotbarStack);
+
+			player.currentScreenHandler.sendContentUpdates();
+			return true;
+		}
+
+		return false;
+	}
+
+	private ItemStack findFirstBlock(PlayerEntity player) {
+		for (int i = 0; i < player.getInventory().size(); i++) {
+			ItemStack stack = player.getInventory().getStack(i);
+			if (stack.getItem() instanceof BlockItem) {
+				return stack;
+			}
+		}
+		return ItemStack.EMPTY;
+	}
+
+	private int findEmptyHotbarSlot(PlayerEntity player) {
+		for (int i = 0; i < 9; i++) {
+			if (player.getInventory().getStack(i).isEmpty()) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	private int findSlotForStack(PlayerEntity player, ItemStack target) {
+		for (int i = 0; i < player.getInventory().size(); i++) {
+			if (player.getInventory().getStack(i) == target) {
+				return i;
+			}
+		}
+		return -1;
 	}
 }
