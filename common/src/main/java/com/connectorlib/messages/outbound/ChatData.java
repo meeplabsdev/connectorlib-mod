6f4d5561b33390527997f951e31c0284eadf13f7
meeplabsdev <meeplabsdev@noreply.users.github.com>
2025-06-28 19:42:07 +0100

    feat: update ChatData to send UUID information along with names

--- common/src/main/java/com/connectorlib/messages/outbound/ChatData.java
+++ common/src/main/java/com/connectorlib/messages/outbound/ChatData.java
@@ -2,16 +2,18 @@ package com.connectorlib.messages.outbound;
 
 import com.connectorlib.BaseMessage;
 
+import java.util.HashMap;
+
 public class ChatData extends BaseMessage {
 	String ip;
 	String message;
-	String sender;
-	String recipient;
+	HashMap<String, String> sender = new HashMap<>(1);
+	HashMap<String, String> recipient = new HashMap<>(1);
 
-	public ChatData(String ip, String message, String sender, String recipient) {
+	public ChatData(String ip, String message, String senderName, String senderUuid, String recipientName, String recipientUuid) {
 		this.ip = ip;
 		this.message = message;
-		this.sender = sender;
-		this.recipient = recipient;
+		this.sender.put(senderName, senderUuid);
+		this.recipient.put(recipientName, recipientUuid);
 	}
 }
