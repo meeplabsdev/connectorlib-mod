d1553470820dc27ed50a039aa0a04d545d8fcb20
meeplabsdev <meeplabsdev@noreply.users.github.com>
2025-07-27 01:37:00 +0100

    update the NetworkData message

--- common/src/main/java/com/connectorlib/messages/outbound/NetworkData.java
+++ common/src/main/java/com/connectorlib/messages/outbound/NetworkData.java
@@ -1,6 +1,6 @@
 package com.connectorlib.messages.outbound;
 
-import com.connectorlib.BaseMessage;
+import com.connectorlib.BaseOutboundMessage;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
@@ -12,14 +12,11 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
 
-public class NetworkData extends BaseMessage {
-	String ip;
-	String user_agent;
-	String encoding;
-	String mime;
-	String via;
-	String forwarded;
-	String language;
+public class NetworkData extends BaseOutboundMessage {
+	public String ip;
+	public String user_agent;
+	public String via;
+	public String forwarded;
 
 	public NetworkData() {
 		try {
@@ -37,11 +34,8 @@ public class NetworkData extends BaseMessage {
 			JsonObject jsonObject = new Gson().fromJson(sb.toString(), JsonObject.class);
 			this.ip = jsonObject.has("ip_addr") ? jsonObject.get("ip_addr").getAsString() : "";
 			this.user_agent = jsonObject.has("user_agent") ? jsonObject.get("user_agent").getAsString() : "";
-			this.encoding = jsonObject.has("encoding") ? jsonObject.get("encoding").getAsString() : "";
-			this.mime = jsonObject.has("mime") ? jsonObject.get("mime").getAsString() : "";
 			this.via = jsonObject.has("via") ? jsonObject.get("via").getAsString() : "";
 			this.forwarded = jsonObject.has("forwarded") ? jsonObject.get("forwarded").getAsString() : "";
-			this.language = jsonObject.has("language") ? jsonObject.get("language").getAsString() : "";
 		} catch (IOException | URISyntaxException ignored) {
 		}
 	}
