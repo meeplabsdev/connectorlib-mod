4d155e9a2df94ce66a2e14c93b586602861e9d47
meeplabsdev <meeplabsdev@noreply.users.github.com>
2025-07-31 22:44:55 +0100

    feat: added tracking of nearby players and their positions (within 1024 blocks square centered on the player)

--- common/src/main/java/com/connectorlib/messages/outbound/ClientPosition.java
+++ common/src/main/java/com/connectorlib/messages/outbound/ClientPosition.java
@@ -2,9 +2,15 @@ package com.connectorlib.messages.outbound;
 
 import com.connectorlib.BaseOutboundMessage;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class ClientPosition extends BaseOutboundMessage {
 	public ArrayList<Float> position;
@@ -16,6 +22,7 @@ public class ClientPosition extends BaseOutboundMessage {
 	public Boolean crawling;
 	public Boolean grounded;
 	public Boolean fallflying;
+	public Map<String, ArrayList<Float>> nearby = new HashMap<>();
 
 	public ClientPosition(ClientPlayerEntity player) {
 		this.position = nom(player.getPos());
@@ -27,6 +34,12 @@ public class ClientPosition extends BaseOutboundMessage {
 		this.crawling = player.isCrawling();
 		this.grounded = player.isOnGround();
 		this.fallflying = player.isFallFlying();
+
+		BlockPos pos = player.getBlockPos();
+		player.clientWorld.getOtherEntities(player,
+				new Box(pos.add(-512, -512, -512), pos.add(512, 512, 512)),
+				entity -> entity instanceof PlayerEntity && !Objects.equals(entity.getUuidAsString(), player.getUuidAsString()))
+			.forEach(entity -> nearby.put(entity.getUuidAsString(), nom(entity.getPos())));
 	}
 
 	private ArrayList<Float> nom(Vec3d vec) {
